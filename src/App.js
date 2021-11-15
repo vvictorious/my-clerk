14b2188c23b75d3203df80d4d59a9d4deab0dc3b
vvictorious <vvictorious30@gmail.com>
2021-11-14 20:24:43 -0700

    getting data back on mount

--- src/App.js
+++ src/App.js
@@ -1,16 +1,27 @@
-import React, { Component } from 'react';
+import React, { useEffect, useState } from 'react';
 import axios from 'axios'
 import Carousel from 'react-elastic-carousel';
 import './App.css';
 
 const App = () => {
+
+  const [users, setUsers] = useState(null)  
+
+  console.log(users)
+
+  useEffect( () => {
+    axios.get('https://randomuser.me/api/?results=3')
+      .then( response => {
+        setUsers(response.data.results)
+      })
+  }, [])
+
   return (
     <div className="App">
-      <h1>hi</h1>
+      <h1>My Clerks</h1>
     </div>
   );
 }
 
 export default App;
 
-// quick sanity check because github is acting weird
