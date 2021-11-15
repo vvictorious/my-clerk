8e30b53c96392cb049c866802c4b410989e5f0b4
vvictorious <vvictorious30@gmail.com>
2021-11-15 00:16:50 -0700

    new users are being fetched once btn is clicked and you are at the end of the users array

--- src/App.js
+++ src/App.js
@@ -1,46 +1,74 @@
 import React, { useEffect, useState } from 'react';
 import axios from 'axios'
 import './App.css';
-import Carousel from 'react-elastic-carousel';
+import Carousel, {consts} from 'react-elastic-carousel';
 import { Card } from './Card'
 
 
 const App = () => {
 
-  const [users, setUsers] = useState(null)  
+  const [loading, setLoading] = useState(false) 
+  const [users, setUsers] = useState(null) 
+  
+  const breakPoints = [
+    {width: 1, itemsToShow: 1},
+    {width: 550, itemsToShow: 2},
+    {width: 768, itemsToShow: 3},
+
+  ]
 
   console.log(users)
 
   useEffect( () => {
-    axios.get('https://randomuser.me/api/?results=5')
+    setLoading(true)
+    axios.get('https://randomuser.me/api/?results=6')
       .then( response => {
         setUsers(response.data.results)
       })
+      .finally( () => {
+        setLoading(false)
+      })
   }, [])
 
-  const fetchMoreUsers = (pageIndex) => {
-    if (pageIndex === users.length - 1) {
-      console.log('fired')
-      axios.get('https://randomuser.me/api/?results=5')
+  const fetchMoreUsers = () => {
+    console.log('fetch more users ran')
+    axios.get('https://randomuser.me/api/?results=6')
       .then( response => {
         response.data.results.forEach( user => {
           setUsers(users => [...users, user])
         })
       })
-    }
   }
 
-  return (
-    <div className="App">
-      <h1>My Clerks</h1>
+  const myArrow = ({ type, onClick, isEdge }) => {
+    const pointer = type === consts.PREV ? '<' : '>'
+      if(isEdge && type === consts.NEXT && users?.length >= 6 && users?.length <= 100) {
+        fetchMoreUsers()
+      }
+    return (
+      <button className='btn' onClick={onClick} disabled={isEdge}>
+        {pointer}
+      </button>
+    )
+  }
+  
 
-      <Carousel onChange={(currentItem, pageIndex) =>
-        fetchMoreUsers(pageIndex)
-      }>
-        {users ? users.map(user => (<Card user={user} key={user.login.uuid} />)) :(<h1>No Users</h1>)}         
-      </Carousel>
-    </div>
-  );
+  if (loading) {
+    return (
+      <h1>Loading...</h1>
+    )
+  } else {
+    return (
+      <div className="App">
+        <h1>My Clerks</h1>
+  
+        <Carousel breakPoints={breakPoints} renderArrow={myArrow}>
+          {users ? users.map(user => (<Card user={user} key={user.login.uuid} />)) :(<h1>No Users</h1>)}         
+        </Carousel>
+      </div>
+    );
+  }
+
 }
 
 export default App;
